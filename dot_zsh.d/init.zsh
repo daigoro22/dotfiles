4c0915876e7fc32358aa482319c166973a34d03d
daigoro <daigoro.abe@systemi.co.jp>
2023-07-24 17:13:19 +0900

    git 補完機能追加

--- dot_zsh.d/init.zsh
+++ dot_zsh.d/init.zsh
@@ -1,1 +1,39 @@
 eval "$(anyenv init -)"
+
+# 補完機能有効にする
+autoload -U compinit
+compinit -u
+ 
+# 補完候補に色つける
+autoload -U colors
+colors
+zstyle ':completion:*' list-colors "${LS_COLORS}"
+ 
+# 単語の入力途中でもTab補完を有効化
+setopt complete_in_word
+# 補完候補をハイライト
+zstyle ':completion:*:default' menu select=1
+# キャッシュの利用による補完の高速化
+zstyle ':completion::complete:*' use-cache true
+# 大文字、小文字を区別せず補完する
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+# 補完リストの表示間隔を狭くする
+setopt list_packed
+ 
+# コマンドの打ち間違いを指摘してくれる
+setopt correct
+SPROMPT="correct: $RED%R$DEFAULT -> $GREEN%r$DEFAULT ? [Yes/No/Abort/Edit] => "
+
+#ブランチ名を表示する
+fpath=(~/.zsh.d $fpath)
+if [ -f ${HOME}/.zsh.d/_git ]; then
+       zstyle ':completion:*:*:git:*' script ~/.zsh.d/_git
+fi
+if [ -f ${HOME}/.zsh.d/git-prompt.sh ]; then
+       source ${HOME}/.zsh.d/git-prompt.sh
+fi
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUPSTREAM=auto
+setopt PROMPT_SUBST ; PS1='[%n@%m %c$(__git_ps1 " (%s)")]\$ '
