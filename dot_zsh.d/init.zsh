1adc78e2f95976172580cb3e0ee265b793aab585
daigoro <daigoro.abe@systemi.co.jp>
2023-11-23 12:56:47 +0900

    ブランチ名の補完が効かなかった問題を修正

--- dot_zsh.d/init.zsh
+++ dot_zsh.d/init.zsh
@@ -24,19 +24,6 @@ setopt list_packed
 setopt correct
 SPROMPT="correct: $RED%R$DEFAULT -> $GREEN%r$DEFAULT ? [Yes/No/Abort/Edit] => "
 
-#ブランチ名を表示する
-fpath=(~/.zsh.d $fpath)
-if [ -f ${HOME}/.zsh.d/_git ]; then
-       zstyle ':completion:*:*:git:*' script ~/.zsh.d/_git
-fi
-if [ -f ${HOME}/.zsh.d/git-prompt.sh ]; then
-       source ${HOME}/.zsh.d/git-prompt.sh
-fi
-GIT_PS1_SHOWDIRTYSTATE=true
-GIT_PS1_SHOWUNTRACKEDFILES=true
-GIT_PS1_SHOWSTASHSTATE=true
-GIT_PS1_SHOWUPSTREAM=auto
-setopt PROMPT_SUBST ; PS1='[%n@%m %c$(__git_ps1 " (%s)")]\$ '
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
